b13edf2c58f09379a1ca62c0329f627abec2fc22
Lenny822 <84620158+Lenny822@users.noreply.github.com>
2023-04-22 10:04:50 +0200

    Create de.lua

---
+++ locales/de.lua
@@ -0,0 +1,18 @@
+Locales['de'] = {
+  ['shop'] = 'Shop',
+  ['shops'] = 'Shops',
+  ['press_menu'] = 'Drücke [E] um auf den ~g~Shop~g~ zuzugreifen.',
+  ['shop_item'] = '%sEUR',
+  ['bought'] = 'Du kaufst ~b~%sx %s~s~ für ~b~%sEUR',
+  ['not_enough'] = 'Du hast ~r~nicht~s~ genügend Geld! Dir Fehlt ~b~%sEUR!',
+  ['player_cannot_hold'] = 'Du hast ~r~nicht~s~ genügend freien Platz in deinem Inventar!',
+  ['shop_confirm'] = 'Willst du %sx %s kaufen für %sEUR?',
+  ['no'] = 'Nein',
+  ['yes'] = 'Ja',
+  ['amount'] = 'Anzahl',
+  ['amount_placeholder'] = 'Anzahl die du kaufen möchtest',
+  ['confirm'] = 'Bestätigen',
+  ['purchase'] = 'Kaufen',
+  ['bread'] = 'Brot',
+  ['water'] = 'Wasser',
+}
